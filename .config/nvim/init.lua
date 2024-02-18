044e83aea9bb23430b6ba3728fe0201c409c4f10
sirncn <guilley.simon@gmail.com>
2024-02-18 18:33:41 +0100

    add lazy.nvim

---
+++ .config/nvim/init.lua
@@ -0,0 +1,10 @@
+require("config.options")
+require("config.keymaps")
+require("config.lazy")
+
+vim.api.nvim_create_autocmd("User", {
+  pattern = "VeryLazy",
+  callback = function()
+    require("config.autocmds")
+  end,
+})
---
+++ .config/nvim/lua/config/keymaps.lua
@@ -0,0 +1,3 @@
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+
