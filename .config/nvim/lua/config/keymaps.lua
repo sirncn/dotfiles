e219a06f25748c412449100ce1b1953e2962e15c
sirncn <guilley.simon@gmail.com>
2024-02-18 18:49:03 +0100

    update lazy.nvim config files

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,14 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
+vim.g.mapleader = " "
+
+local keymap = vim.keymap
+
+---------------------
+-- General Keymaps
+---------------------
+
+keymap.set("i", "jk", "<ESC>", { desc = "exit insert mode" })
+keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "clear search highlights" })
+keymap.set("n", "x", '"_x', { desc = "delete single character without copying into register" })
--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,3 +1,58 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- text encoding
+vim.scriptencoding = "utf-8"
+vim.opt.encoding = "utf-8"
+vim.opt.fileencoding = "utf-8"
+
+-- line numbers
+vim.wo.number = true -- show absolute line number
+vim.opt.relativenumber = true -- show relative line number
+
+vim.opt.title = true
+
+-- indentation
+vim.opt.shiftwidth = 2 -- N of spaces for indent width
+vim.opt.tabstop = 2 -- N of spaces for tabs
+vim.opt.autoindent = true -- copy indent from prev line
+vim.opt.smartindent = true
+
+-- search settings
+vim.opt.ignorecase = true -- ignore case
+vim.opt.smartcase = true -- case-sensitive when using mixed case
+vim.opt.hlsearch = false -- disable highlight for search term
+vim.opt.incsearch = true
+
+-- clipboard
+vim.opt.clipboard:append("unnamedplus") -- use system clipboard as default register
+
+vim.opt.backup = false
+vim.opt.showcmd = true
+vim.opt.cmdheight = 0
+vim.opt.laststatus = 2
+vim.opt.expandtab = true
+vim.opt.scrolloff = 10
+vim.opt.shell = "bash"
+vim.opt.backupskip = { "/tmp/*", "/private/tmp/*" }
+vim.opt.inccommand = "split"
+vim.opt.smarttab = true
+vim.opt.breakindent = true
+vim.opt.wrap = false -- No Wrap lines
+vim.opt.backspace = { "start", "eol", "indent" } -- allow backspace on indent, end of line or insert mode start position
+vim.opt.path:append({ "**" }) -- Finding files - Search down into subfolders
+vim.opt.wildignore:append({ "*/node_modules/*" })
+
+-- Undercurl
+vim.cmd([[let &t_Cs = "\e[4:3m"]])
+vim.cmd([[let &t_Ce = "\e[4:0m"]])
+
+-- Turn off paste mode when leaving insert
+vim.api.nvim_create_autocmd("InsertLeave", {
+  pattern = "*",
+  command = "set nopaste",
+})
+
+-- Add asterisks in block comments
+vim.opt.formatoptions:append({ "r" })
