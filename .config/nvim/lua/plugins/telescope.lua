dc201f85455b0cce59219ed0abac0acc128e5bb1
sirncn <guilley.simon@gmail.com>
2024-04-16 19:40:24 +0200

    change nvim-telescope config

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -13,59 +13,29 @@ Plugin.cmd = { "Telescope" }
 function Plugin.init()
   vim.keymap.set(
     "n",
-    "<leader>tf",
+    "<leader>tff",
     "<cmd>Telescope find_files<cr>",
     { desc = "find files within current working directory" }
   )
   vim.keymap.set(
     "n",
-    "<leader>ts",
+    "<leader>tfs",
     "<cmd>Telescope live_grep<cr>",
     { desc = "find string in current working directory as you type" }
   )
   vim.keymap.set(
     "n",
-    "<leader>tc",
+    "<leader>tfc",
     "<cmd>Telescope grep_string<cr>",
     { desc = "find string under cursor in current working directory" }
   )
   vim.keymap.set(
     "n",
-    "<leader>tb",
+    "<leader>tlb",
     "<cmd>Telescope buffers<cr>",
     { desc = "list open buffers in current neovim instance" }
   )
-  vim.keymap.set("n", "<leader>tt", "<cmd>Telescope help_tags<cr>", { desc = "list available help tags" })
-  vim.keymap.set(
-    "n",
-    "<leader>th",
-    "<cmd>Telescope file_browser<cr>",
-    { desc = "list files within current working directory" }
-  )
-  vim.keymap.set(
-    "n",
-    "<leader>gc",
-    "<cmd>Telescope git_commits<cr>",
-    { desc = "list all git commits (use <cr> to checkout) ['gc' for git commits]" }
-  )
-  vim.keymap.set(
-    "n",
-    "<leader>gfc",
-    "<cmd>Telescope git_bcommits<cr>",
-    { desc = "list git commits for current file/buffer (use <cr> to checkout) ['gfc' for git file commits]" }
-  )
-  vim.keymap.set(
-    "n",
-    "<leader>gb",
-    "<cmd>Telescope git_branches<cr>",
-    { desc = "list git branches (use <cr> to checkout) ['gb' for git branch]" }
-  )
-  vim.keymap.set(
-    "n",
-    "<leader>gs",
-    "<cmd>Telescope git_status<cr>",
-    { desc = "list current changes per file with diff preview ['gs' for git status]" }
-  )
+  vim.keymap.set("n", "<leader>tlt", "<cmd>Telescope help_tags<cr>", { desc = "list available help tags" })
 end
 
 local function opts()
